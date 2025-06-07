2c84872c5d468f757a42ee28133365017a649bb9
dhirajkumbhar28 <134613068+dhirajkumbhar28@users.noreply.github.com>
2025-06-07 21:15:38 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://leetlab-production.up.railway.app/api/v1",
   withCredentials: true,
 });
